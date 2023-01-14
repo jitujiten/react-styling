7f4afd4bd2a97f2d381273682e6c77fe1b3602ea
jitujiten <sahuohjitu@gmail.com>
2023-01-14 18:06:43 +0530

    added task-6 /using ref

--- src/Component/Ui/Card.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Card.css";
-
-const Card=(props)=>{
-    const card="card "+props.className;
-    return <div className={card}>{props.children}</div>
-}
-
-export default Card ;
